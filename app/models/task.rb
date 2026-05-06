08fadd6cae6e38e58a42bfc98efc2a8cd49680df
SampabloC <sampablo-jisc@outlook.com>
2026-05-05 18:02:50 -0600

    User and Task models

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,16 @@
 class Task < ApplicationRecord
   belongs_to :user
+
+  STATUSES = %w[pending in_progress completed]
+
+  validates :title, presence: true
+  validates :status, inclusion: { in: STATUSES }
+
+  after_initialize :set_default_status, if: :new_record?
+
+  private
+
+  def set_default_status
+    self.status ||= "pending"
+  end
 end
