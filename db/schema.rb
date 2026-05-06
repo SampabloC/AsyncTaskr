428f6d5087b104fdfa7d74264c9c7123feeb8bb2
SampabloC <sampablo-jisc@outlook.com>
2026-05-05 18:02:50 -0600

    User and Task models

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_27_230747) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_06_000126) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "tasks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "status"
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "tasks", "users"
 end
