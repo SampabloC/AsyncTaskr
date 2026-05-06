ed9055cf1303c8ed75e131283e4e46b18b726ee0
SampabloC <sampablo-jisc@outlook.com>
2026-05-05 22:16:59 -0600

    Task controller and enpoints Updated routes

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,46 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :update, :destroy]
+
+  def index
+    tasks = current_user.tasks
+    render json: tasks
+  end
+
+  def show
+    render json: @task
+  end
+
+  def create
+    task = current_user.tasks.build(task_params)
+
+    if task.save
+      render json: task, status: :created
+    else
+      render json: { errors: task.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      render json: @task
+    else
+      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    render json: { message: "Task was successfully destroyed." }
+  end
+
+  private
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def task_params
+    params.require(:tasks).permit(:title)
+  end
+end
