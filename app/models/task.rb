2d0a9b934270f3559cfefdd77d2872b840154fa9
SampabloC <sampablo-jisc@outlook.com>
2026-08-05 19:55:35 -0600

    Remove comments and unused code.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
 
-  # STATUSES = %w[pending in_progress completed failed]
-
   enum :status, {
     pending: "pending",
     in_progress: "in_progress",
